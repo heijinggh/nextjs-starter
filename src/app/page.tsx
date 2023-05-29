b6df894a304366977ffb655a7550954c057988fc
HeiJing <heijing.miner@gmail.com>
2023-05-29 14:23:33 +0700

    feat: add layout demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center p-24'>
+		<main className='flex flex-col items-center p-24'>
 			<div>Hello</div>
 			<Link href='/playground/counter'>Counter</Link>
 		</main>
--- src/app/playground/counter/page.tsx
+++ src/app/playground/counter/page.tsx
@@ -2,7 +2,7 @@ import Counter from '@/components/business/counter';
 
 export default function CounterPage() {
 	return	(
-		<div className='min-h-screen'>
+		<div className=''>
 			<h1>This is the counter page!</h1>
 			<Counter />
 		</div>
