472653eff2dc003643bd3bf86f318800f3358a72
HeiJing <heijing.miner@gmail.com>
2023-05-29 11:44:33 +0700

    feat: add counter link to index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center p-24'>
 			<div>Hello</div>
+			<Link href='/playground/counter'>Counter</Link>
 		</main>
 	);
 }
